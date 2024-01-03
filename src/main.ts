581e1c2c3697ada2ff78c8ca2ae230e31b5b0dad
Yarolav Gaponov <yarolav.gaponov@gmail.com>
2024-01-03 19:18:32 +0100

    chore: updated

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,23 @@
 import "reflect-metadata";
 import { banner } from "./banner";
 import { HttpServerFactory } from "./http-server";
+import { userInfo } from "os";
 
 banner();
 
+process.once("uncaughtException", () => process.exit(-1));
+
+const info = userInfo();
+if (info.uid === 0 || info.gid === 0) {
+    console.warn("WARNING: You are running this application as root. This is not recommended.");
+}
+
+
 const httpServer = HttpServerFactory.create();
-httpServer.start();
 
 process.once("SIGINT", async () => {
     await httpServer.stop();
+    process.exit(0);
 });
+
+httpServer.start();
